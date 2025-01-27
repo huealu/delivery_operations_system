3b724aee468daadc0b7db77fdd6573ff09ab8def
Helen Dinh <helendinh@Helens-MBP.attlocal.net>
2025-01-27 07:33:56 -0600

    add post processing code

--- base_models.py
+++ base_models.py
@@ -1,11 +1,13 @@
 import numpy as np
 import lightgbm as lgbm
+import matplotlib.pyplot as plt
 
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
 from lightgbm import LGBMRegressor
-
+from sklearn.neural_network import MLPRegressor
+from sklearn.metrics import root_mean_squared_error
 
 import time
 
@@ -43,21 +45,59 @@ def grid_search_model(model, X_train, y_train):
 def LGBM_model(X_train, y_train):
     """Train the model using LightGBM."""
     # Create the model
-    model = LGBMRegressor(learning_rate=0.1, max_depth=7, n_estimators=200, metric='rmse')
+    model = LGBMRegressor(learning_rate=0.1, 
+                          max_depth=7, 
+                          n_estimators=200, 
+                          metric='rmse')
     
     # Train the model
     model.fit(X_train, y_train)
-    
     return model
 
 
-def print_model_results(model, X_test, y_test):
+def print_feature_importance_plot(model, X_train):
+    """Print a feature importance plot."""
+    # Get the feature importances
+    # Plot feature importance using Gain
+    lgbm.plot_importance(model, importance_type="gain", figsize=(7,6), title="LightGBM Feature Importance (Gain)")
+
+    # Plot feature importance using Split
+    lgbm.plot_importance(model, importance_type="split", figsize=(7,6), title="LightGBM Feature Importance (Split)")
+
+    # Plot feature importance 
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    fig.suptitle('Horizontally stacked subplots')
+    ax1 = lgbm.plot_importance(model, importance_type="gain", figsize=(7,6), title="LightGBM Feature Importance (Gain)")
+    ax2 = lgbm.plot_importance(model, importance_type="split", figsize=(7,6), title="LightGBM Feature Importance (Split)")
+    
+    # Show the plot
+    plt.show()
+
+
+def MLP_model(X_train, y_train):
+    """Train the model using Multi-Layer Perceptron."""
+    # Create the model
+    model = MLPRegressor(hidden_layer_sizes=(100, 50, 25), 
+                         max_iter=1000, 
+                         alpha=0.01, 
+                         learning_rate='adaptive', 
+                         random_state=42)
+    
+    # Train the model
+    model.fit(X_train, y_train)
+    return model
+
+
+
+def print_model_results(model, X_train, y_train, X_test, y_test):
     """Print the model results."""
-    # Predict the test data
-    y_pred = model.predict(X_test)
+    # Generate prediction for train and test data
+    y_pred_train = model.predict(X_train)
+    y_pred_test = model.predict(X_test)
+    
+    # Calculate the RMSE for train data
+    rmse = root_mean_squared_error(y_train, y_pred_train)
     
     # Print the results
-    print("R2 score:", model.score(X_test, y_test))
-    print("MAE:", np.mean(np.abs(y_pred - y_test)))
-    print("MSE:", np.mean((y_pred - y_test) ** 2))
-    print("RMSE:", np.sqrt(np.mean((y_pred - y_test) ** 2)))
+    print("MSE:", np.mean((y_pred_test - y_test) ** 2))
+    print("RMSE:", np.sqrt(np.mean((y_pred_test - y_test) ** 2)))
